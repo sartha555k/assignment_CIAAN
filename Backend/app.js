53ab250cc27a58731ff17787091c7c2af5c08cb7
Sarthak Patel <sarthak.code30@gmail.com>
2025-08-02 16:19:31 +0530

    database connection

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,17 @@
+require('dotenv').config();
 const express = require("express");
+require('dotenv').config();
+const connectDB = require("./config/database");
 const app = express();
-app.use((req , res)=>{
-    res.send("Hello World !!");
-})
-
-app.listen(3000 , () =>{
-    console.log("Server is running on port 3000");
-})
+const http = require("http");
+const server = http.createServer(app);
+connectDB()
+  .then(() => {
+    console.log("Database connected successfully");
+    server.listen(3000, () => {
+      console.log("Server is running successfully on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection failed", err);
+  });
